37cbb7b43be4234fe4befed3ed0726563fa16a93
Dobroslawa Toranska <dianatoranska@Dianas-MacBook-Air-2.local>
2019-12-01 22:26:55 +0000

    setting up react hooks

---
+++ fetch/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+	const [ name, setName ] = useState('');
+	const [ userName, setUsername ] = useState('');
+	const [ followers, setFollowers ] = useState('');
+	const [ following, setFollowing ] = useState('');
+	const [ repos, setRepos ] = useState('');
+	const [ avatar, setAvatar ] = useState('');
+	const [ userInput, setUserInput ] = useState('');
+	const [ error, setError ] = useState('');
+
+	return (
+		<div>
+			<div className="navbar">Github Search</div>
+		</div>
+	);
+}
+
+export default App;
