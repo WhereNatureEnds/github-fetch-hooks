2871dd5a3f1bdf2a07e8146ada3efef0506d1319
Dobroslawa Toranska <dianatoranska@Dianas-MacBook-Air-2.local>
2019-12-01 22:57:44 +0000

    fetching an example user

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Form } from 'semantic-ui-react';
+import { Form, Card, Image, Icon } from 'semantic-ui-react';
 
 function App() {
 	const [ name, setName ] = useState('');
@@ -12,6 +12,12 @@ function App() {
 	const [ userInput, setUserInput ] = useState('');
 	const [ error, setError ] = useState('');
 
+	useEffect(() => {
+		fetch('https://api.github.com/users/example').then((res) => res.json()).then((data) => {
+			console.log(data);
+		});
+	}, []);
+
 	return (
 		<div>
 			<div className="navbar">Github Search</div>
@@ -19,9 +25,27 @@ function App() {
 				<Form>
 					<Form.Group>
 						<Form.Input placeholder="Search Github User" name="name" />
-						<Form.Button contenct="Submit" />
+						<Form.Button contenct="Search" />
 					</Form.Group>
 				</Form>
+			</div>
+			<div className="card">
+				<Card>
+					<Image src="/images/avatar/large/matthew.png" wrapped ui={false} />
+					<Card.Content>
+						<Card.Header>Matthew</Card.Header>
+						<Card.Meta>
+							<span className="date">Joined in 2015</span>
+						</Card.Meta>
+						<Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+					</Card.Content>
+					<Card.Content extra>
+						<a>
+							<Icon name="user" />
+							22 Friends
+						</a>
+					</Card.Content>
+				</Card>
 			</div>
 		</div>
 	);
